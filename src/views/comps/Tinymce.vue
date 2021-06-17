94720d71664828087229ce330fd0d2b4d8456916
liaomeirong <liaomeirong@wondersgroup.com>
2021-06-17 16:10:59 +0800

    tinymce init

--- src/views/comps/Tinymce.vue
+++ src/views/comps/Tinymce.vue
@@ -22,7 +22,7 @@ export default {
       init: {
         language_url: '/tinymce/langs/zh_CN.js',// 语言包的路径
         language: 'zh_CN',//语言
-        skin_url: 'tinymce/skins/ui/oxide',// skin路径
+        // skin_url: 'tinymce/skins/ui/oxide',// skin路径
         height: 300,//编辑器高度
         branding: false,//是否禁用“Powered by TinyMCE”
         menubar: true,//顶部菜单栏显示,
