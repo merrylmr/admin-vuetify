092259c3c51f2eb14df8c0f0e095a28b6ea8588c
liaomeirong <merrylmr@qq.com>
2021-11-01 11:43:04 +0800

    merge

--- vue.config.js
+++ vue.config.js
@@ -3,17 +3,7 @@ const PrerenderSPAPlugin = require('prerender-spa-plugin');
 const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 const IsProd = process.env.NODE_ENV === 'production';
 module.exports = {
-    configureWebpack: (config) => {
-        const oneOfsMap = config.module.rule('scss').oneOfs.store
-        oneOfsMap.forEach(item => {
-            item
-                .use('sass-resources-loader')
-                .loader('sass-resources-loader')
-                .options({
-                    resources: [path.resolve(__dirname, 'src/assets/scss/mixin.scss'), path.resolve(__dirname, 'src/assets/scss/variables.scss')]
-                })
-                .end()
-        })
+    configureWebpack: () => {
         const plugins = IsProd ? [
             new PrerenderSPAPlugin({
                 staticDir: path.join(__dirname, 'dist'),
@@ -42,5 +32,17 @@ module.exports = {
             }
 
         }
+    },
+    chainWebpack: config => {
+        const oneOfsMap = config.module.rule('scss').oneOfs.store
+        oneOfsMap.forEach(item => {
+            item
+                .use('sass-resources-loader')
+                .loader('sass-resources-loader')
+                .options({
+                    resources: [path.resolve(__dirname, 'src/assets/scss/mixin.scss'), path.resolve(__dirname, 'src/assets/scss/variables.scss')]
+                })
+                .end()
+        })
     }
 }
