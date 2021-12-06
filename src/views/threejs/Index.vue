d3d4e32a46d5e7696d8e893dfafc49a6f7c9bd8e
liaomeirong <merrylmr@qq.com>
2021-12-06 18:02:56 +0800

    热点 ing

--- src/views/threejs/Index.vue
+++ src/views/threejs/Index.vue
@@ -31,13 +31,26 @@
         <div class="view-area"
              id="container">
         </div>
-        <div class="help-frame">
+
+        <div class="help-frame"
+             v-if="$route.name==='view'">
           <v-btn
               class="btn"
               color="primary"
               small
               @click="setCameraPosHandle">把当前视觉设置为初始视角
           </v-btn>
+        </div>
+        <!--热点列表-->
+        <div class="hotSpot-list"
+             v-if="$route.name==='hot' && !isLoading">
+          <div class="hotStop-item"
+               @mousedown="pointMoveStartHandle($event,item)"
+               v-for="(item,index) in hotSpots"
+               :style="transformStyle(item.pos)"
+               :key="index">
+            <img :src="item.iconPath">
+          </div>
         </div>
       </div>
       <div class="right-sidebar">
@@ -151,6 +164,10 @@ import PreviewDlg from './comps/Preview.vue'
 import html2canvas from "html2canvas";
 import HotSpot from './comps/HotSpot.vue'
 // TODO: 水平和垂直的角度需要再修改修改
+// 热点：
+// 第一步：实现可以拖拽
+// 第二步：位置坐标转化，随着鼠标的滚动、拖拽
+// 第三步： camera旋转时候， 位置改变（世界坐标系转屏幕坐标系）
 export default {
   name: 'editor-3d',
   data() {
@@ -168,6 +185,7 @@ export default {
         maxAzimuthAngle: 180,
       },
       camera: null,
+      container: null,
       cameraPos: {
         x: 0,
         y: 0,
@@ -191,10 +209,33 @@ export default {
           icon: 'mdi-map-marker-outline'
         }
       ],
-      activeName: 'view'
+      activeName: 'view',
+      // 热点列表
+      hotSpots: [
+        {
+          iconPath: 'img/new_spotd1_gif.png',
+          pos: {
+            x: 0,
+            y: 0,
+            z: 0
+          }
+        }
+      ],
+      isLoading: true
     }
   },
   components: {PreviewDlg, HotSpot},
+  computed: {
+    transformStyle() {
+      return (point) => {
+        const pos = this.worldVector2Screen(point);
+        return {
+          transform: `translate(${pos.x}px,${pos.y}px) translate(-40px,-40px)`
+        }
+      }
+
+    }
+  },
   methods: {
     createThumbnail() {
       if (this.$route.name !== 'view') return
@@ -212,6 +253,7 @@ export default {
 
     },
     async init() {
+      this.isLoading = true;
       const container = document.getElementById('container');
 
       const width = container.clientWidth;
@@ -226,7 +268,7 @@ export default {
 
       const scene = new THREE.Scene();
       const camera = new THREE.PerspectiveCamera(this.params.fov, width / height, this.params.near, this.params.far);
-      camera.position.z = 0.01;
+      camera.position.set(0, 0, 0.1)
 
       this.camera = camera;
 
@@ -254,13 +296,13 @@ export default {
 
       await renderModel('3d/images/scene.jpeg');
       render();
+      this.isLoading = false;
 
       function render() {
         renderer.render(scene, camera);
         //  获取当前视觉的坐标（x,y,z）
       }
 
-      controls.addEventListener('change', render);
 
       function resizeHandle() {
         console.log('resizeHandle1111')
@@ -273,6 +315,32 @@ export default {
       }
 
       window.addEventListener('resize', resizeHandle);
+
+      // 辅助坐标系  参数250表示坐标系大小，可以根据场景大小去设置
+      const axisHelper = new THREE.AxisHelper(25000);
+      scene.add(axisHelper);
+
+      this.container = container;
+      return {scene, camera, controls, renderer, container}
+    },
+
+    renderHotPoints(scene) {
+      const pointTexture = new THREE.TextureLoader().load('img/new_spotd1_gif.png');
+      const material = new THREE.SpriteMaterial({map: pointTexture});
+
+      let poiObjects = []
+      for (let i = 0; i < this.hotSpots.length; i++) {
+        let sprite = new THREE.Sprite(material);
+        sprite.scale.set(0.1, 0.1, 0.1);
+
+        const position = this.hotSpots[i].pos;
+        console.log('position:', position)
+        sprite.position.set(position.x, position.y, position.z)
+
+        scene.add(sprite)
+        poiObjects.push(sprite);
+      }
+      return poiObjects
     },
     // 角度转弧度
     degToRad(deg) {
@@ -299,6 +367,8 @@ export default {
     // 设置当前视觉
     setCameraPosHandle() {
       this.cameraPos = this.camera.position;
+      // this.camera.updateProjectionMatrix();
+      // console.log('this.camera', this.camera);
       this.createThumbnail()
     },
     changeMenu(item) {
@@ -306,12 +376,118 @@ export default {
       this.$router.push({
         name: item.value
       })
-    }
+    },
+    //TODO: 边界情况处理
+    pointMoveStartHandle(e) {
+      // 操作的是样式（style.transform）
+      console.log('e.currentTarget', e.currentTarget)
+      const target = e.currentTarget;
+      console.log('target', target.style.transform);
+      let transform = target.style.transform;
+      const reg = /translate\((-?\d+(?:\.\d*)?)px, (-?\d+(?:\.\d*)?)px\)/;
+      transform = transform.match(reg);
+
+
+      let translateX = parseInt(transform[1]);
+      let translateY = parseInt(transform[2]);
+
+      console.log('translateX', translateX)
+      console.log('translateY', translateY)
+      e.preventDefault();
+      let startPos = {
+        x: e.clientX,
+        y: e.clientY
+      }
+
+      const mouseMoveHandle = (e) => {
+        console.log('mouseMoveHandle:', e)
+
+        const diffX = e.clientX - startPos.x;
+        const diffY = e.clientY - startPos.y;
+
+        translateX += diffX;
+        translateY += diffY;
+
+        startPos = {
+          x: e.clientX,
+          y: e.clientY
+        }
+
+        console.log('diffX', diffX, diffY)
+        target.style.transform = `translate(${translateX}px,${translateY}px) translate(-40px,-40px)`
+      }
+      const mouseUpHandle = (e) => {
+        console.log('mouseUpHandle:', e)
+        // 转化成世界坐标，存储
+        document.body.removeEventListener('mousemove', mouseMoveHandle)
+        document.body.removeEventListener('mouseup', mouseUpHandle)
+      }
+      document.body.addEventListener('mousemove', mouseMoveHandle)
+      document.body.addEventListener('mouseup', mouseUpHandle)
+    },
+    // 矢量坐标转化成平面坐标点
+    vectorToPos(vector3) {
+      console.log('vector', vector3);
+      const width = window.innerWidth - 51 - 230;
+      const height = window.innerHeight - 41;
+      console.log('width:', width, 'height:', height);
+      // const sceneCenter = this.cameraPos;
+    },
+    worldVector2Screen(center) {
+      // 相对于相机的位置（{x:0,y:0,z:0.1}）
+      const cameraPos = this.camera.position;
+
+      const worldVector = new THREE.Vector3(center.x, center.y, center.z);
+      const stdVector = worldVector.project(this.camera);
+      console.log('stdVector:', stdVector);
+      const a = this.container.clientWidth / 2;
+      const b = this.container.clientHeight / 2;
+
+      console.log('a:', a, 'b:', b)
+      const x = Math.round(stdVector.x * a + a);
+      const y = Math.round(stdVector.y * b + b);
+
+      console.log('this.camera.position', cameraPos);
+
+      const worldVector1 = new THREE.Vector3(
+          Number((cameraPos.x).toFixed(16)),
+          Number((cameraPos.y).toFixed(16)),
+          Number((cameraPos.z).toFixed(16)));
+      const stdVector1 = worldVector1.project(this.camera);
+      console.log('stdVector1', stdVector1)
+      const x1 = Math.round(stdVector1.x * a + a);
+      const y1 = Math.round(stdVector1.y * b + b);
+
+      console.log('x:', x, 'y:', y)
+      console.log('x1:', x1, 'y1:', y1)
+      return {x, y}
+    },
+
+    // transformStyle(point) {
+    //   const pos = this.worldVector2Screen(point);
+    //   return {
+    //     transform: `translate(${pos.x}px,${pos.y}px) translate(-40px,-40px)`
+    //   }
+    // },
   },
   mounted() {
     this.activeName = this.$route.name;
     this.$nextTick(async () => {
-      await this.init();
+      const {scene, camera, controls, renderer} = await this.init();
+
+      // const poiObjects = this.renderHotPoints(scene)
+
+      function changeHandle() {
+        renderer.render(scene, camera);
+        console.log('camera.position', camera.position)
+
+        // console.log('poiObjects', poiObjects, camera)
+        // poiObjects[0].scale.set(0.2, 0.2, 0.2)
+      }
+
+      controls.addEventListener('change', changeHandle);
+
+
       this.createThumbnail();
     })
   },
@@ -375,7 +551,7 @@ export default {
   .stage {
     flex: auto;
     height: 100%;
-    padding: 20px;
+    //padding: 20px;
     position: relative;
 
     .view-area {
@@ -402,6 +578,27 @@ export default {
         transform: translate(-50%, 0);
         pointer-events: auto;
       }
+    }
+
+    .hotSpot-list {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      user-select: none;
+      pointer-events: none;
+    }
+
+    .hotStop-item {
+      position: absolute;
+      border: 3px solid orange;
+      border-radius: 5px;
+      cursor: pointer;
+      width: 80px;
+      height: 80px;
+      transform-origin: 50% 50%;
+      pointer-events: auto;
     }
   }
 
--- src/views/threejs/comps/HotSpot.vue
+++ src/views/threejs/comps/HotSpot.vue
@@ -1,16 +1,166 @@
 <template>
   <div>
-    <v-btn class="ma-2" tile color="indigo" dark>添加热点</v-btn>
+    <v-btn class="ma-2" tile color="indigo" dark
+           @click="drawer=!drawer">添加热点
+    </v-btn>
     <v-divider></v-divider>
     <!-- 当前场景热点 -->
     <div class="list">
 
     </div>
+
+    <v-navigation-drawer
+        v-model="drawer"
+        height="100vh"
+        :right="true"
+        :hide-overlay="true"
+        absolute
+        temporary>
+      <div class="pa-4">
+        <div class="header">
+          <div class="header-title">添加热点</div>
+          <div class="header-close">
+            <!--          <v-icon v-text="dmi-"></v-icon>-->
+          </div>
+        </div>
+        <div class="body">
+          <v-form>
+            <v-select
+                :items="items"
+                v-model="form.iconType"
+                item-text="label"
+                item-value="value"
+                label="图标">
+            </v-select>
+
+
+            <!-- 系统图表列表-->
+            <div class="sys-icon-list">
+              <div class="icon-item"
+                   v-for="(item,index) in sysIcons"
+                   :class="{'is-active':form.iconPath===item.key}"
+                   :key="index">
+                <img :src="item.url">
+              </div>
+            </div>
+            <v-subheader>图标大小</v-subheader>
+            <v-slider
+                thumb-label="always"
+                v-model="form.iconSize"
+                min="-100"
+                max="100"
+            ></v-slider>
+
+
+            <v-select
+                :items="hotTypes"
+                item-text="label"
+                item-value="value"
+                v-model="form.hotType"
+                label="热点类型">
+            </v-select>
+
+          </v-form>
+        </div>
+      </div>
+    </v-navigation-drawer>
   </div>
 </template>
 
 <script>
 export default {
   name: 'hot-spot',
+  data() {
+    return {
+      drawer: true,
+      items: [
+        {
+          label: '系统图标',
+          value: 'sys'
+        },
+        {
+          label: '自定义图标',
+          value: 'custom'
+        }
+      ],
+      hotTypes: [
+        {
+          label: '全景切换',
+          value: 'scene'
+        },
+        {
+          label: '超链接',
+          value: 'link'
+        },
+        {
+          label: '图片热点',
+          value: 'img'
+        },
+        {
+          label: '视频热点',
+          value: 'video'
+        },
+        {
+          label: '图文热点',
+          value: 'list'
+        },
+        {
+          label: '环物热点',
+          value: 'around'
+        },
+        {
+          label: '文章热点',
+          value: 'article'
+        }
+      ],
+      sysIcons: [
+        {
+          key: 'forward',
+          url: 'img/new_spotd1_gif.png'
+        },
+        {
+          key: 'left',
+          url: 'img/new_spotd2_gif.png'
+        }],
+      form: {
+        iconType: 'sys',
+        iconPath: 'forward',
+        iconSize: 10,
+        hotType: 'scene',
+        params: {}
+      }
+    }
+  }
 }
 </script>
+
+
+<style scoped lang="scss">
+.header {
+  &-title {
+    font-size: 16px;
+  }
+}
+
+.sys-icon-list {
+  @include flex();
+  flex-wrap: wrap;
+}
+
+.icon-item {
+  width: 35px;
+  height: 35px;
+  margin: 0 2px 15px;
+  cursor: pointer;
+
+  &.is-active {
+    border: 1px solid #5e35b1;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+</style>
